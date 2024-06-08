6f38282a964dd42842df7f5d78cf71291f2dc37f
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-06-08 15:41:10 +0530

    add text animation,loading state

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -4,18 +4,19 @@ import loginDark from '@/assets/login-dark.png';
 import loginLight from '@/assets/login-light.png';
 import logoutDark from '@/assets/logout-dark.png';
 import logoutLight from '@/assets/logout-light.png';
-import menubarDark from '@/assets/menu-dark.png';
-import menubarLight from '@/assets/menu-light.png';
+// import menubarDark from '@/assets/menu-dark.png';
+// import menubarLight from '@/assets/menu-light.png';
 import shareDark from '@/assets/share-dark.png';
 import shareLight from '@/assets/share-light.png';
-import { fonts } from '@/fonts/fonts';
-import { AppStore } from '@/store/appStore';
+
 import { signOut, useSession } from 'next-auth/react';
 import { useTheme } from 'next-themes';
 import { Link } from 'next-view-transitions';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import ThemeToggler from './ThemeToggler';
+import { AppStore } from '@/store/appStore';
+import Logo from './Logo';
 
 export default function Appbar() {
   const store = AppStore();
@@ -40,20 +41,16 @@ export default function Appbar() {
     <div className="fixed top-0 left-0 w-full bg-appbar h-[10vh] flex items-center justify-between px-[1.5rem]">
       <div className="flex items-center gap-[2rem]">
         <div className="cursor-pointer">
-          <Image
+          {/* <Image
             src={currentTheme === 'light' ? menubarLight : menubarDark}
             alt="menuBar"
             className="w-[1.4rem] h-[1.4rem]"
             width={22}
             height={22}
-          />
+          /> */}
         </div>
-        <Link
-          className={`${fonts.calSans} text-[2rem] text-primary p-0 cursor-pointer`}
-          href="/"
-        >
-          xkill
-        </Link>
+
+        <Logo isRedirect={true} size={3}></Logo>
       </div>
       <div className="flex items-center gap-[2rem]">
         <div className="cursor-pointer">
